b9869b4816aac1b6b8ef0f295dc2af8b6ae290fe
Ubuntu <ubuntu@ip-172-31-88-112.ec2.internal>
2019-04-08 19:17:17 +0000

    k

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
-    
-]
+#STATICFILES_DIRS = [
+#	os.path.join(BASE_DIR, 'assets'),
+
+#]
