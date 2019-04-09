1a933c6d2db728363c75530796dca75d399ed836
Grassyeagle <eli.crutchfield@gmail.com>
2019-04-09 19:20:35 +0000

    fixed api4

--- weather/api/views.py
+++ weather/api/views.py
@@ -2,12 +2,12 @@ from rest_framework import generics
 
 from .models import Temperature
 from .serializers import TemperatureSerializer
-# Create your views here.
-class TemperatureList(generics.ListAPIView):
+
+class TemperatureList(generics.ListCreateAPIView):
     queryset = Temperature.objects.all()
     serializer_class = TemperatureSerializer
-
-
+    
 class TemperatureDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Temperature.objects.all()
     serializer_class = TemperatureSerializer
+
