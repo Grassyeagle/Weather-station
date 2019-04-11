5b5c4759344e0e3187422ccc1895df154c4d0467
Grassyeagle <eli.crutchfield@gmail.com>
2019-04-11 19:45:08 +0000

    added varieable temp

--- weather/api/views.py
+++ weather/api/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics
 
 from .models import Temperature
 from .serializers import TemperatureSerializer
-
 class TemperatureList(generics.ListCreateAPIView):
     queryset = Temperature.objects.all()
     serializer_class = TemperatureSerializer
--- weather/main/views.py
+++ weather/main/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-
+from api.models import Temperature
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    # Find the newest single temperature
+    temp =   Temperature.objects.order_by('record_time').first()
+    # Gather the total number of temperature readings gathered.
+    tcount = Temperature.objects.count()
+    # Find the first temperature entry recorded
+    tfirst = Temperature.objects.order_by('record_time').first()
+    
+    return render(request, 'home.html', {
+        'temp': temp,
+         'tcount': tcount, 
+         'tfirst': tfirst.record_time}
+)
