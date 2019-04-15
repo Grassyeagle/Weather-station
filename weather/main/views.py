48a1513761fff6accdebfde86a13c34bd735ceb5
Grassyeagle <eli.crutchfield@gmail.com>
2019-04-15 19:32:44 +0000

    typo!!

--- weather/main/views.py
+++ weather/main/views.py
@@ -4,7 +4,7 @@ from api.models import Temperature
 # Create your views here.
 def home(request):
     # Find the newest single temperature
-    temp =   Temperature.objects.order_by('record_time').first()
+    temp =   Temperature.objects.order_by('-record_time').first()
     # Gather the total number of temperature readings gathered.
     tcount = Temperature.objects.count()
     # Find the first temperature entry recorded
