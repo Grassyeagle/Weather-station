5e42aeb0853b4d6c1c0ab3399713f9211d17c8ed
Grassyeagle <eli.crutchfield@gmail.com>
2019-05-06 19:14:29 +0000

    min/max

--- weather/main/views.py
+++ weather/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from api.models import Temperature , H , Bp
+from datetime import datetime, timedelta
+from django.db.models import Max, Min
 
-
+def c2f(celsius):
+    return ((celsius*9/5)+32)
 # Create your views here.
 def home(request):
     # Find the newest single temperature
@@ -21,8 +24,14 @@ def home(request):
     Pcount = Bp.objects.count()
     # Find the first temperature entry recorded
     Pfirst = Bp.objects.order_by('record_time').first()
-    
-    
+    now = datetime.now()
+    some_time_ago = now - timedelta(days=7)
+    tmax = c2f(Temperature.objects.filter(record_time__range=(some_time_ago, now)).aggregate(Max('celsius'))['celsius__max'])
+    hmax = H.objects.filter(record_time__range=(some_time_ago, now)).aggregate(Max('rh'))['rh__max']
+    pmax = Bp.objects.filter(record_time__range=(some_time_ago, now)).aggregate(Max('p'))['p__max']
+    hmin = H.objects.filter(record_time__range=(some_time_ago, now)).aggregate(Min('rh'))['rh__min']
+    pmin = Bp.objects.filter(record_time__range=(some_time_ago, now)).aggregate(Min('p'))['p__min']
+    tmin = c2f(Temperature.objects.filter(record_time__range=(some_time_ago, now)).aggregate(Min('celsius'))['celsius__min'])
     return render(request, 'home.html', {
         'temp': temp,
          'tcount': tcount, 
@@ -33,6 +42,12 @@ def home(request):
          'p': p ,
          'Pcount': Pcount, 
          'Pfirst': Pfirst.record_time,
+         'tmax':tmax,
+         'hmax':hmax,
+         'pmax':pmax,
+         'tmin':tmin,
+         'hmin':hmin,
+         'pmin':pmin,
     }
 )
 
